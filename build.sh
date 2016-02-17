8af080e136d12be43cd3ca4a447ed5c46e7cf900
Jason Rivers <jason@jasonrivers.co.uk>
2016-02-17 14:20:35 +0000

    Add Travis build

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+DOCKER_RUN_IMAGE=nagios
+
+docker build -t ${DOCKER_RUN_IMAGE} .
+
+docker images 
+docker run -d --name ${DOCKER_RUN_IMAGE} -p 80:80 -t ${DOCKER_RUN_IMAGE}
+docker ps -a
+
