88d89e69723e21c7c6e290878e0ffdd980b5b483
Emiliano Gabrielli <emiliano.gabrielli@musement.com>
2017-09-01 12:22:04 +0200

    A bit of shell best practices, mainly vars quote

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ set -e
 
 DOCKER_RUN_IMAGE=nagios
 
-docker build -t ${DOCKER_RUN_IMAGE} .
+docker build -t "${DOCKER_RUN_IMAGE}" .
 
-docker images 
-docker run -d --name ${DOCKER_RUN_IMAGE} -p 80:80 -t ${DOCKER_RUN_IMAGE}
+docker images
+docker run -d --name "${DOCKER_RUN_IMAGE}" -p 80:80 -t "${DOCKER_RUN_IMAGE}"
 docker ps -a
 
