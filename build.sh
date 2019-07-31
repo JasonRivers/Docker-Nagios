c246790f44bb051f8084a864fe6316d3d1f841ff
Jason Rivers <jason@jasonrivers.co.uk>
2019-07-31 11:25:59 +0100

    Update to Nagios 4.4.4

--- build.sh
+++ build.sh
@@ -6,6 +6,5 @@ DOCKER_RUN_IMAGE=nagios
 docker build -t "${DOCKER_RUN_IMAGE}" .
 
 docker images
-docker run -d --name "${DOCKER_RUN_IMAGE}" -p 80:80 -t "${DOCKER_RUN_IMAGE}"
-docker ps -a
+docker run -d --rm --name "${DOCKER_RUN_IMAGE}" -p 8080:80 -t "${DOCKER_RUN_IMAGE}"
 
